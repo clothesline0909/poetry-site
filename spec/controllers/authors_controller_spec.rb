2a7de6af362c7e796a8120cd4f1b6bc7811df2d9
Brett Clark <clarkbab@gmail.com>
2017-06-20 12:47:26 +1000

    Added GET /authors spec.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
+  render_views
 
   # LAZY-LOADED OBJECTS
 
@@ -21,6 +22,26 @@ RSpec.describe AuthorsController, type: :controller do
     it "returns HTTP status OK" do
       get :index
       expect(response).to have_http_status :ok
+    end
+
+    it "renders index template" do
+      get :index
+      expect(response).to render_template 'authors/index'
+    end
+
+    it "returns 3 authors" do
+      get :index
+      expect(JSON.parse(response.body, symbolize_names: true)[:data].length).to be 3
+    end
+
+    it "returns author attributes" do
+      get :index
+      expect(JSON.parse(response.body, symbolize_names: true)[:data].first[:attributes][:name]).to eq authors.first.name
+    end
+
+    it "doesn't return author relationships" do
+      get :index
+      expect(JSON.parse(response.body, symbolize_names: true)[:data].first[:relationships]).to be nil
     end
   end
 end
